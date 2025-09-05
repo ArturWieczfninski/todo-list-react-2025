6d566bfbe83478631f5f9616ba6feac1c4d5376a
Artur Wieczfninski <artur.wieczfninski@gmail.com>
2025-09-05 22:55:45 +0200

    1

---
+++ ArturWieczfninski/todolistreact2025.git/static/js/main.6cfad5ed.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * Entry point for your React ToDo List application.
+ * Add your React code here.
+ */
+
+
+// Example App component
+function App() {
+    return (
+        <div>
+            <h1>ToDo List 2025</h1>
+            {/* Add your components here */}
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
